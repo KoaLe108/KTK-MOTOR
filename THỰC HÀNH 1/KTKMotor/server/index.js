ecbb3036cadf1dd2bf40a952e13a53dc051202b5
Your Name <your.email@example.com>
2026-04-19 20:47:01 +0700

    Update Final

--- THỰC HÀNH 1/KTKMotor/server/index.js
+++ THỰC HÀNH 1/KTKMotor/server/index.js
@@ -1,7 +1,22 @@
 //CLI: npm install express body-parser--save
+const path = require('path');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// deployment
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+
+// '/' serve the files at client-customer/build/* as static files
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'))
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
