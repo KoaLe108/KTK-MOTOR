b0749eccde28e3e6a46f3cce17ad77555ae98061
Your Name <your.email@example.com>
2026-04-19 20:16:29 +0700

    Update mail & pass

--- THỰC HÀNH 1/KTKMotor/server/utils/MyConstants.js
+++ THỰC HÀNH 1/KTKMotor/server/utils/MyConstants.js
@@ -3,8 +3,8 @@ const MyConstants = {
     DB_USER: 'ktk123',
     DB_PASS: 'ktk123',
     DB_DATABASE: 'KTKMotor',
-    EMAIL_USER: 'koale041@gmail.com', // Microsoft mail service
-    EMAIL_PASS: 'vkjd uyrx inve qzbt',
+    EMAIL_USER: 'ktk.motor123@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'fcqg lqkr nchi ffvl', // Microsoft mail service app password
     JWT_SECRET: 'b88953d90316533457629e2792203f08df417f09ef6bec5aab976300d82392019a117e8aa973db5d21c389cae447ed8e66fc4f3fb2c90d4dfc3c90f20a43d8a3',
     JWT_EXPIRES: '3600000', // in milliseconds
 };
